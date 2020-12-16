fd05f82661ed7d704161ff5d90ca594d65ddf229
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-16 16:11:52 +0400

    Initial commit

---
+++ src/part1/lesson05/task01/Main.java
@@ -0,0 +1,26 @@
+package part1.lesson05.task01;
+//import java.util.arrays;
+public class Main {
+
+    public static void main(String[] args) {}
+
+    public void toLeft(){
+
+        int[][] a = {{0,2,3,4,5,6,7,8,9,10},{0,2,3,4,5,6,7,8,9,10}};
+        for ( int i =1; i<a.length;i++)
+            System.out.println(i);
+        //System.out.println(arrays.tostring(a));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
