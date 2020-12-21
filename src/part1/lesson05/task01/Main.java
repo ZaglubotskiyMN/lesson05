592e54bf4d824ab94e8c1a9c42bcac5f43136ed8
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-21 18:56:22 +0400

    Zadanie №1

--- src/part1/lesson05/task01/Main.java
+++ src/part1/lesson05/task01/Main.java
@@ -1,15 +1,31 @@
 package part1.lesson05.task01;
-//import java.util.arrays;
+
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        toLeft();
 
-    public void toLeft(){
+    }
+
+
+    public static void toLeft(){
 
         int[][] a = {{0,2,3,4,5,6,7,8,9,10},{0,2,3,4,5,6,7,8,9,10}};
-        for ( int i =1; i<a.length;i++)
-            System.out.println(i);
-        //System.out.println(arrays.tostring(a));
+
+
+        for ( int i =0; i<a.length;i++){
+            for (int j=0;j<a[i].length;j++)
+           System.out.println(Arrays.deepToString(a));
+
+        }
+
+
+        //System.out.print(""+a[i][j]+ "");
+        // System.out.println(a[0][4]);
+        //System.out.print(Arrays.toString(a));
+
 
 
 
